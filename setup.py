2af0c5d2aa9b282b325a1fd1f7b73639f1b1dde5
Daan Debie <debie.daan@gmail.com>
2017-12-03 16:09:51 +0100

    Remove Python 3.3 support

--- setup.py
+++ setup.py
@@ -87,7 +87,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
