44f3b1d61ac1f60e04e0487c8a30edba3ac70cbe
Sean Johnson <pirogoeth@users.noreply.github.com>
2019-11-08 16:04:00 -0600

    Make machine core startup more flexible (#2)

--- machine/bin/run.py
+++ machine/bin/run.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import signal
 import sys
 import os
-from functools import partial
 
-from loguru import logger
-
-from machine import Machine
+from machine import core
 
 
 def main():
@@ -17,40 +13,6 @@ def main():
     sys.path.insert(0, os.getcwd())
 
     loop = asyncio.new_event_loop()
+    core.start(loop)
 
-    # Handle INT and TERM by gracefully halting the event loop
-    for s in (signal.SIGINT, signal.SIGTERM):
-        loop.add_signal_handler(s, partial(prepare_to_stop, loop))
-
-    bot = Machine(loop=loop)
-    loop.run_until_complete(bot.run())
-
-    # Once `prepare_to_stop` returns, `loop` is guaranteed
-    # to be stopped.
-    prepare_to_stop(loop)
-
-    loop.close()
     sys.exit(0)
-
-
-def prepare_to_stop(loop: asyncio.AbstractEventLoop):
-    # Calling `cancel` on each task in the active loop causes
-    # a `CancelledError` to be thrown into each wrapped coroutine during
-    # the next iteration, which _should_ halt execution of the coroutine
-    # if the coroutine does not explicitly suppress the `CancelledError`.
-    for task in asyncio.Task.all_tasks(loop=loop):
-        task.cancel()
-
-    # Using `ensure_future` on the `stop` coroutine here ensures that
-    # the `stop` coroutine is the last thing to run after each cancelled
-    # task has its `CancelledError` emitted.
-    asyncio.ensure_future(stop(), loop=loop)
-    loop.run_forever()
-
-
-# @asyncio.coroutine
-async def stop():
-    loop = asyncio.get_event_loop()
-    logger.info("Thanks for playing!")
-
-    loop.stop()
--- machine/core.py
+++ machine/core.py
@@ -2,7 +2,9 @@
 
 import asyncio
 import inspect
+import signal
 import sys
+from functools import partial
 from typing import Mapping, Optional
 
 import dill
@@ -17,6 +19,49 @@ from machine.slack import MessagingClient
 from machine.storage import PluginStorage
 from machine.utils import aio, collections, log_propagate
 from machine.utils.module_loading import import_string
+
+__all__ = ["Machine", "start"]
+
+
+def start(loop: asyncio.AbstractEventLoop, settings: Optional[dict] = None):
+    """ Given an `asyncio.AbstractEventLoop` and optional `settings` dictionary,
+        create and run a `Machine` instance to completion.
+    """
+
+    # Handle INT and TERM by gracefully halting the event loop
+    for s in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(s, partial(_prepare_to_stop, loop))
+
+    bot = Machine(loop=loop, settings=settings)
+    loop.run_until_complete(bot.run())
+
+    # Once `prepare_to_stop` returns, `loop` is guaranteed
+    # to be stopped.
+    _prepare_to_stop(loop)
+
+    loop.close()
+
+
+def _prepare_to_stop(loop: asyncio.AbstractEventLoop):
+    # Calling `cancel` on each task in the active loop causes
+    # a `CancelledError` to be thrown into each wrapped coroutine during
+    # the next iteration, which _should_ halt execution of the coroutine
+    # if the coroutine does not explicitly suppress the `CancelledError`.
+    for task in asyncio.Task.all_tasks(loop=loop):
+        task.cancel()
+
+    # Using `ensure_future` on the `stop` coroutine here ensures that
+    # the `stop` coroutine is the last thing to run after each cancelled
+    # task has its `CancelledError` emitted.
+    asyncio.ensure_future(_stop(), loop=loop)
+    loop.run_forever()
+
+
+async def _stop():
+    loop = asyncio.get_event_loop()
+    logger.info("Thanks for playing!")
+
+    loop.stop()
 
 
 class Machine:
