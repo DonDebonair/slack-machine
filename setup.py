333a85938ef0a1a3a533e0fa9714d364df2928d1
Daan Debie <daan@dv.email>
2021-09-12 23:26:05 +0200

    chore: update Python versions for PyPI and bump package version

--- setup.py
+++ setup.py
@@ -90,6 +90,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Communications :: Chat",
         "Topic :: Internet",
