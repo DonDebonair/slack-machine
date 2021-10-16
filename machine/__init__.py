c99924611e560649a3cd7c07e9d0afc451d25692
Daan Debie <daan@dv.email>
2021-10-16 17:43:30 +0200

    feat: switch to Poetry for dependency and project management

--- machine/__init__.py
+++ machine/__init__.py
@@ -1,8 +1,5 @@
 from .core import Machine
-from .__about__ import (__title__, __description__, __uri__, __version__, __author__,
-                        __email__, __license__, __copyright__)
 
 __all__ = [
-    '__title__', '__description__', '__uri__', '__version__', '__author__', '__email__',
-    '__license__', '__copyright__', 'Machine'
+    'Machine'
 ]
