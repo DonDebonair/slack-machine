93957928f367b19492e9b4b797af40d380bdba82
Richard Clark <richard@craftypenguins.net>
2023-06-11 22:17:21 -0700

    Fix up test for sqlite plugin, add expire handling

--- machine/storage/backends/sqlite.py
+++ machine/storage/backends/sqlite.py
@@ -1,14 +1,24 @@
+from __future__ import annotations
+
 import aiosqlite
+import time
 from machine.storage.backends.base import MachineBaseStorage
 from typing import Any, Mapping
+from contextlib import AsyncExitStack
 
 
 class SQLiteStorage(MachineBaseStorage):
-    def __init__(self, settings: Mapping[str, Any]):
-        self._file = settings.get("SQLITE_FILE")
+    _context_stack: AsyncExitStack
 
-    async def _connect(self):
+    def __init__(self, settings: Mapping[str, Any]):
+        self._file = settings.get("SQLITE_FILE", "slack-machine-state.db")
+
+    async def close(self) -> None:
+        await self.conn.close()
+
+    async def init(self) -> None:
         self.conn = await aiosqlite.connect(self._file)
+        self.conn.text_factory = bytes
         self.cursor = await self.conn.cursor()
         await self.cursor.execute("""
             CREATE TABLE IF NOT EXISTS storage (
@@ -19,8 +29,7 @@ class SQLiteStorage(MachineBaseStorage):
         """)
         await self.conn.commit()
 
-    async def set(self, key: str, value: str, expires: int = None):
-        await self._connect()
+    async def set(self, key: str, value: bytes, expires: int | None = None) -> None:
         await self.cursor.execute(
             """
             INSERT OR REPLACE INTO storage (key, value, expires)
@@ -29,34 +38,30 @@ class SQLiteStorage(MachineBaseStorage):
             (key, value, expires),
         )
         await self.conn.commit()
-        await self.conn.close()
 
-    async def get(self, key: str):
-        await self._connect()
-        await self.cursor.execute("SELECT value FROM storage WHERE key=?", (key,))
+    async def get(self, key: str) -> bytes | None:
+        current_GMT = int(time.time())
+        await self.cursor.execute("SELECT value FROM storage WHERE key=? and (expires > ? OR expires IS NULL)", (key, current_GMT))
         row = await self.cursor.fetchone()
-        await self.conn.close()
         return row[0] if row else None
 
-    async def delete(self, key: str):
-        await self._connect()
+    async def delete(self, key: str) -> None:
         await self.cursor.execute("DELETE FROM storage WHERE key=?", (key,))
         await self.conn.commit()
-        await self.conn.close()
 
-    async def close(self):
-        pass
-
-    async def has(self, key: str):
-        await self._connect()
-        await self.cursor.execute("SELECT EXISTS(SELECT 1 FROM storage WHERE key=?)", (key,))
+    async def has(self, key: str) -> bool:
+        current_GMT = int(time.time())
+        await self.cursor.execute(
+            "SELECT EXISTS(SELECT 1 FROM storage WHERE key=? and (expires > ? OR expires IS NULL))", (key, current_GMT)
+        )
         result = await self.cursor.fetchone()
-        await self.conn.close()
-        return bool(result[0])
+        if result is not None:
+            return result[0]
+        return False
 
-    async def size(self):
-        await self._connect()
+    async def size(self) -> int:
         await self.cursor.execute("SELECT COUNT(*) FROM storage")
         result = await self.cursor.fetchone()
-        await self.conn.close()
-        return result[0]
+        if result is not None:
+            return result[0]
+        return 0
